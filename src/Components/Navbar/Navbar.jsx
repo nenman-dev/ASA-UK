189d089b4989d73a9f891fd23ddb0914f11be0c8
Glory Nenman Kitchinme <glorie.kitch@gmail.com>
2025-06-30 02:59:10 +0100

    mobile_menu

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import {useState} from 'react'
 import './Navbar.css'
 import logo from '../../assets/logo.png'
-import menu_icon from '../../assets/menu-icon.png'
-
+import menu_icon from '../../assets/menu_icon.png'
+import close_icon from '../../assets/close_icon.png'
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] =useState(false);
   return (
-    <nav>
+    <nav className= {`navbar ${isOpen ? "open" : ""}`}>
+      <header>
         <img src= {logo} alt="" className='logo'/>
-        <ul>
-            <li>WHO WE ARE</li>
-            <li>WHAT WE DO</li>
-            <li>EVENT</li>
-            <li>JOIN & SUPPORT</li>
-            <li>NEWS</li>
-            <li><button className='btn orange'>BECOME A MEMBER</button></li>
-            <li><button className='btn green'>DONATE</button></li>
+      </header>
+      <div className='inner'> 
+       <button 
+          className="menu-toggle" 
+          onClick={() => setIsOpen(!isOpen)}> 
+          <img src={isOpen ? close_icon : menu_icon } className="icon" />
+        </button> 
+        <ul className={`nav-links ${isOpen ?'show' : ""}`}>
+          <li><Link to = 'hero' smooth= {true} offset={0} duration={500}>WHO WE ARE</Link></li>
+          <li><Link to = 'about' smooth= {true} offset={0} duration={500}>WHAT DO WE DO</Link></li>
+          <li><Link to = 'events' smooth= {true} offset={0} duration={500}>EVENTS</Link></li>
+          <li><Link to = 'pillars' smooth= {true} offset={0} duration={500}>JOIN & SUPPORT</Link></li>
+          <li><Link to = 'latest' smooth= {true} offset={0} duration={500}>NEWS</Link></li>
+          <li><Link to = 'brands' smooth= {true} offset={0} duration={500}className='btn orange'>BECOME A MEMBER</Link></li>
+          <li><Link to = 'top-footer' smooth= {true} offset={0} duration={500}className='btn green'>DONATE</Link></li>
         </ul>
+      </div>
     </nav>
   )
 }
