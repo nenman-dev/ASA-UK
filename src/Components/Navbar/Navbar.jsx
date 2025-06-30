ed4a8fe3d500a814eb18f10a8b4093336026d956
Glory Nenman Kitchinme <glorie.kitch@gmail.com>
2025-06-30 03:38:31 +0100

    about-mobile

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -20,12 +20,12 @@ const Navbar = () => {
         </button> 
         <ul className={`nav-links ${isOpen ?'show' : ""}`}>
           <li><Link to = 'hero' smooth= {true} offset={0} duration={500}>WHO WE ARE</Link></li>
-          <li><Link to = 'about' smooth= {true} offset={0} duration={500}>WHAT DO WE DO</Link></li>
-          <li><Link to = 'events' smooth= {true} offset={0} duration={500}>EVENTS</Link></li>
-          <li><Link to = 'pillars' smooth= {true} offset={0} duration={500}>JOIN & SUPPORT</Link></li>
-          <li><Link to = 'latest' smooth= {true} offset={0} duration={500}>NEWS</Link></li>
-          <li><Link to = 'brands' smooth= {true} offset={0} duration={500}className='btn orange'>BECOME A MEMBER</Link></li>
-          <li><Link to = 'top-footer' smooth= {true} offset={0} duration={500}className='btn green'>DONATE</Link></li>
+          <li><Link to = 'about' smooth= {true} offset={-50} duration={500}>WHAT DO WE DO</Link></li>
+          <li><Link to = 'events' smooth= {true} offset={-150} duration={500}>EVENTS</Link></li>
+          <li><Link to = 'pillars' smooth= {true} offset={-120} duration={500}>JOIN & SUPPORT</Link></li>
+          <li><Link to = 'latest' smooth= {true} offset={-600} duration={500}>NEWS</Link></li>
+          <li><Link to = 'brands' smooth= {true} offset={-130} duration={500}className='btn orange'>BECOME A MEMBER</Link></li>
+          <li><Link to = 'top-footer' smooth= {true} offset={170} duration={500}className='btn green'>DONATE</Link></li>
         </ul>
       </div>
     </nav>
--- src/Components/Topbar/Topbar.jsx
+++ src/Components/Topbar/Topbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Topbar.css'
 import search_icon from '../../assets/search.svg'
+import { Link } from 'react-scroll';
 
 const Topbar = () => {
   return (
     <div className='topbar'>
         <ul className='topbar-links'>
-            <li className='topbar-contact'>Contact Us</li>
+            <li><Link to = 'main-footer' smooth= {true} offset={-130} duration={500} className='topbar-contact'>Contact Us</Link></li>
             <li>Members Login</li>
             <li><span><img src={search_icon} alt=''/></span>Search</li>
         </ul>
