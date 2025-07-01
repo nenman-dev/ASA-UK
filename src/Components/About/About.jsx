2a8ff5142957d9e877ec77bd912f3989fa5830dd
Glory Nenman Kitchinme <glorie.kitch@gmail.com>
2025-07-01 21:40:35 +0100

    hover

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './About.css'
 import about_img from '../../assets/about.png'
 import white_arrow from '../../assets/arrow.svg'
-import dark_arrow from '../../assets/dark_arrow.png'
+import dark_arrow from '../../assets/dark_arrow1.png'
 import play_icon from '../../assets/play.svg'
 
 
@@ -18,7 +18,7 @@ const About = ({setPlayState}) => {
             <p>The Royal African Society is Britain's prime Africa organisation.</p>
             <p>Our goal is to promote Africa in business, politics, culture and academia. We are a membership society that works to foster better understanding and strong relationships between Britain, Africa and the world.</p>
             <p>Our in-depth knowledge of the continent and our collaborative approach to programmes and partnerships make us the first choice for anyone who wants to engage, understand, celebrate or learn more about Africa. </p>
-            <button className='btn'>KNOW MORE <img src= {white_arrow} alt=""/> <img src= {dark_arrow} alt=""/></button>
+            <button className='btn'>KNOW MORE <img src= {white_arrow} alt=""/> <img src= {dark_arrow} alt="" className='dark_arrow'/></button>
         </div>
     </div>
   )
--- src/Components/PillarCard/PillarCard.jsx
+++ src/Components/PillarCard/PillarCard.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './PillarCard.css'
 
 const PillarCard = ({image, label, arrowIcon}) => {
+  const [rotate, setRotate]=useState(false);
+
+  const handleClick = () => {
+    setRotate(prev => !prev);
+  };
+
    return (
     <div className='gallery-card'>
       <img src={image} alt={label} />
       <div className='content'>
         <p>{label}</p>
-        <img src={arrowIcon} alt="arrow" className='arrow' />
+        <img src={arrowIcon} alt="arrow" className= {`arrow ${rotate ? 'rotate' : ''}`} onClick={handleClick}  />
       </div>
     </div>
   );
